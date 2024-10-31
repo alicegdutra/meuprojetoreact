7b5dc3c3057f60f2ef309c9979dab730e7ed084d
Alice Gimenez Dutra <alicegzdutra@gmail.com>
2024-10-31 12:47:58 -0300

    ajuste

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,13 +1,12 @@
 // Atv02.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Letreiro from './Letreiro';
+
 
 function Atv02() {
   return (
     <>
       <h2>Atividade 02</h2>
-      <Letreiro />
       <Link to="/">Voltar</Link>
     </>
   );
